1a3bc6b50b75995b157c3a93099078b70acb145e
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-27 12:37:56 +0100

    Add all necessary migration files with foreign keys and indexes included

---
+++ db/migrate/20220427111907_add_user_ref_to_posts.rb
@@ -0,0 +1,6 @@
+class AddUserRefToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_foreign_key :posts, :users, column: :author_id, primary_key: :id
+    add_index :posts, :author_id
+  end
+end
