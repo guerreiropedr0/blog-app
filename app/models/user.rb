55f45b11192d30d188cc8bb1d327e779bfe0b67f
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-02 12:22:00 +0100

    Add recent_posts custom method to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  def recent_posts
+    Post.where(author_id: id).limit(3)
+  end
 end
