5064e663601ce32968cb11d5be3f78a20fbc2bc9
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-02 14:18:11 +0100

    Add increment_counter method on Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def increment_counter
+    post = Post.find_by(id: post_id)
+    if post.comments_counter
+      post.update(comments_counter: post.comments_counter + 1)
+    else
+      post.update(comments_counter: 1)
+    end
+  end
 end
