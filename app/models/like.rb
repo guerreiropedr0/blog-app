c48e98adb9555023cc358012c32cf81ddb5ca431
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-02 14:22:00 +0100

    Add increment_counter method on Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def increment_counter
+    post = Post.find_by(id: post_id)
+    if post.likes_counter
+      post.update(likes_counter: post.likes_counter + 1)
+    else
+      post.update(likes_counter: 1)
+    end
+  end
 end
