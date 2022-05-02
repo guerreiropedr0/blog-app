8fa4a24628e78203d44875b4b5b63a0c9c2778cd
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-02 14:10:25 +0100

    Add increment_counter method on Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :author, class_name: 'User'
+
+  def increment_counter
+    user = User.find_by(id: author_id)
+    if user.posts_counter
+      user.update(posts_counter: user.posts_counter + 1)
+    else
+      user.update(posts_counter: 1)
+    end
+  end
 end
