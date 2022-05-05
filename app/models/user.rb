cb411fc8b9365da173afe7e2548228178d02108b
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-05 21:47:09 +0100

    Add validation for model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, length: { minimum: 1 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def recent_posts
     posts.order('created_at Desc').limit(3)
   end
