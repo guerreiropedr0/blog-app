9c47ec37a320cebdfca5e631b47ead91c5828e33
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-05 23:08:48 +0100

    Change navbar into partial and add partial flash for displaying flash notices and alerts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   after_save :increment_post_counter
-  
+
   validates :title, length: { minimum: 1, maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
