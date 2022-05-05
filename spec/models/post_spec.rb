9295a94cced3a0058955e407fe0de499cd599532
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-05 21:53:41 +0100

    Add tests for validation on  model Post (TDD)

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) do
+    @user = User.create(name: 'Pedro Guerreiro', photo: 'random_link.png', bio: 'I\'m creating a test right now!')
+    @post = Post.create(author_id: @user.id, title: 'Controller Spec', text: 'It\'s exciting!')
+  end
+
+  describe 'Title' do
+    it 'should return invalid since it needs characters' do
+      @post.title = ''
+
+      expect(@post).to_not be_valid
+    end
+
+    it 'should return invalid since it has more than 250 characters' do
+      @post.title = 'a' * 251
+      expect(@post).to_not be_valid
+    end
+
+    it 'should return valid since it\'s between 0 and 250 characters' do
+      @post.title = 'a' * 50
+      expect(@post).to be_valid
+    end
+  end
+
+  describe 'comments_counter' do
+    it 'should return invalid since it need to be an integer greater than or equal to zero' do
+      @post.comments_counter = -1
+
+      expect(@post).to_not be_valid
+    end
+
+    it 'should return valid' do
+      expect(@post).to be_valid
+    end
+  end
+
+  describe 'likes_counter' do
+    it 'should return invalid since it need to be an integer greater than or equal to zero' do
+      @post.likes_counter = -1
+
+      expect(@post).to_not be_valid
+    end
+
+    it 'should return valid' do
+      expect(@post).to be_valid
+    end
+  end
 end
