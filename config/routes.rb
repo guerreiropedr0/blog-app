824f2f1733d2b65d99fa9381da9d5965f20a03b6
racidelaid <racidelaid@gmail.com>
2022-05-10 13:47:57 +0100

    add authorization for deleting comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:new, :create]
     end
   end
