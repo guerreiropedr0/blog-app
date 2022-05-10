5ea089be709af825c2c23fa3c540a43c87eac8a0
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-10 11:30:07 +0100

    Add authorization for deleting post, both the admin and the author of the post can delete it

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,13 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :manage, Post, author_id: user.id
+      can :read, :all
+    end
+  end
 end
