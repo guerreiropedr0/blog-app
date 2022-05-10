022f28d9524ed2e17e5b3df32f4b3db891cc4123
racidelaid <racidelaid@gmail.com>
2022-05-10 13:50:57 +0100

    decrement comments count on destroy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,9 +3,15 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :increment_comment_counter
+  after_destroy :decrement_comment_counter
 
   def increment_comment_counter
     post = Post.find(post_id)
     post.increment!(:comments_counter)
+  end
+
+  def decrement_comment_counter
+    post = Post.find(post_id)
+    post.decrement!(:comments_counter)
   end
 end
