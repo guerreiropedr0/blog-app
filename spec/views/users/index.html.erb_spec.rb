1242a51d63447723f1748cb7abc7a8feda3fb8a3
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-11 10:50:09 +0100

    Add integration tests for users index

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe 'users/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: :system do
+  context 'index page' do
+    before(:each) do
+      @user = User.create(name: 'Pedro Guerreiro', photo: 'https://i.pravatar.cc/150?img=12',
+                          bio: 'I\'m creating a test right now!',
+                          email: 'test@domain.com')
+      @user.confirm
+      visit users_path
+    end
+
+    it 'should show welcoming message' do
+      expect(page).to have_content('Welcome to Blog App')
+    end
+
+    it 'should show username of user/users' do
+      expect(page).to have_content('Pedro Guerreiro')
+    end
+
+    it 'should show image of user/users' do
+      expect(page).to have_css("img[src*='i.pravatar.cc/150?img=12']")
+    end
+
+    it 'should show number of posts of user/users' do
+      expect(page).to have_content('Number of posts 0')
+    end
+
+    it 'should click open profile button and redirect to that user\'s show page' do
+      click_link('Open Profile')
+      expect(current_path).to eql(user_path(@user.id))
+    end
+  end
 end
