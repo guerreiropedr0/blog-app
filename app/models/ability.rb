13680a12667ea1d8e31c8b71090f1fb8d24bd22d
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-11 09:10:03 +0100

    Fix comment bug

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
       can :manage, :all
     else
       can :manage, Post, author_id: user.id
-      can :destroy, Comment, author_id: user.id
+      can :manage, Comment, author_id: user.id
       can :read, :all
     end
   end
