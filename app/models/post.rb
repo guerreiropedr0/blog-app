fb9c2b728197b2277e407d26410928ddd80405ad
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-11 09:16:55 +0100

    Add feature where if post has comments or likes, when deleting post, comments and like are also deleted from that post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   belongs_to :author, class_name: 'User'
 
   after_save :increment_post_counter
